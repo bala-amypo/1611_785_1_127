e6314e02493f361a51abc7218feb525548ab060c
bala-amypo <bala@amypo.in>
2025-12-21 07:48:44 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,4 +1,4 @@
-package com.example.demo.service.Impl;
+package com.example.demo.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.stereotype.Service;
@@ -9,31 +9,31 @@ import com.example.demo.repository.UserRepo;
 @Service
 
 public class UserServiceImpl implements UserService{
-    @Autowired StudentRepo student;
+    @Autowired UserRepo student;
     //save()
     //findAll()
     //findById()
     //deleteById();
     //existById();
     @Override
-    public StudentEntity postData(StudentEntity stu){
+    public User postData(User stu){
         return student.save(stu);
     }
     @Override
-    public List<StudentEntity>getAllData(){
+    public List<User>getAllData(){
         return student.findAll();
     }
     @Override
-    public String DeleteData(int id){
+    public String DeleteData(long id){
         student.deleteById(id);
         return "Deleted Successfully";
     }
     @Override
-    public StudentEntity getData(int id){
+    public User getData(long id){
         return student.findById(id).orElse(null);
     }
     @Override
-    public StudentEntity updateData(int id,StudentEntity entity){
+    public User updateData(long id,User entity){
         if(student.existsById(id)){
             entity.setId(id);
             return student.save(entity);
