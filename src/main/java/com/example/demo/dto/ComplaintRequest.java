240701b4519a10b92bd397275b262a28469cb482
bala-amypo <bala@amypo.in>
2025-12-25 15:59:59 +0000

    User commit

--- src/main/java/com/example/demo/dto/ComplaintRequest.java
+++ src/main/java/com/example/demo/dto/ComplaintRequest.java
@@ -1,15 +1,41 @@
 package com.example.demo.dto;
 
-import com.example.demo.entity.Complaint;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter @Setter
 public class ComplaintRequest {
+
     private String title;
     private String description;
-    private String category;
-    private String channel;
-    private Complaint.Severity severity;
-    private Complaint.Urgency urgency;
+    private Long userId;
+
+    public ComplaintRequest() {
+    }
+
+    public ComplaintRequest(String title, String description, Long userId) {
+        this.title = title;
+        this.description = description;
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
 }
