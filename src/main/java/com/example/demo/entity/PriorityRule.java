255107e9c3fcafe9c18d6e48940c05c76bfdec32
bala-amypo <bala@amypo.in>
2025-12-25 17:53:16 +0000

    User commit

--- src/main/java/com/example/demo/entity/PriorityRule.java
+++ src/main/java/com/example/demo/entity/PriorityRule.java
@@ -1,31 +1,20 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
+@Data               // Generates getters, setters, toString, equals, hashCode
+@NoArgsConstructor   // Generates a no-args constructor
+@AllArgsConstructor  // Generates an all-args constructor
 public class PriorityRule {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String ruleName;
-
-    private String description;
-
-    private Integer weight;
-
-    private boolean active = true;
-
-    @ManyToMany(mappedBy = "priorityRules")
-    private Set<Complaint> complaints = new HashSet<>();
+    private String name;
+    private int priority;
 }
