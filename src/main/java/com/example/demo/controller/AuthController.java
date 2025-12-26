a67ed0c4fa49fc7a7b607db7badabfc289c3af00
bala-amypo <bala@amypo.in>
2025-12-26 14:03:25 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -8,11 +8,21 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/auth")
 public class AuthController {
 
-    @PostMapping("/login")
-    public AuthResponse login(@RequestBody AuthRequest req) {
-        return new AuthResponse();
-    }
+@Autowired
+    private PasswordEncoder encoder;
 
+    @PostMapping("/login")
+    public String login(@RequestBody AuthRequest request) {
+        ExtraStudent student = ser.CheckEmail(request.getEmail());
+        if (student == null) {
+            throw new RuntimeException("User Not Found");
+
+        }
+        if (!encoder.matches(request.getPassword(), student.getPassword())) {
+            throw new RuntimeException("Invalid credentials");
+        }
+
+        return "Login successful for " + student.getEmail();
     @PostMapping("/register")
     public String register() {
         return "registered";
