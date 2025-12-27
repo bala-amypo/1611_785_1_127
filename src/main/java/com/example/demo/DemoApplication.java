03ec709aedbf3799e971020662dc6d1a7124a46a
bala-amypo <bala@amypo.in>
2025-12-27 09:19:50 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		system.out.println(new BcryptPasswordEncoder().encode(""))
+		system.out.println(new BCryptPasswordEncoder().encode("code"));
 	}
 
 }
